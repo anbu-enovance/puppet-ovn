5121a4e9d7eaf18f82faca01c6dd00e61d86377d
Babu Shanmugam <bschanmu@redhat.com>
2016-11-15 12:05:49 +0000

    Input DB listen IP address for the northd
    When ovn-northd is started, along with it ovsdb servers for the southbound
    and northbound databases are also started. Without additional options for the
    northd service, the DB servers will be listenting on 0.0.0.0
    
    This patch creates a systemd environment file that will create the db listen
    options for the northd service.

--- manifests/northd.pp
+++ manifests/northd.pp
@@ -1,10 +1,23 @@
 # ovn northd
 # == Class: ovn::northd
 #
-# installs ovn package starts the ovn-northd service
+# Installs ovn package starts the ovn-northd service
 #
-class ovn::northd() {
+# [*dbs_listen_ip*]
+#   The IP-Address where OVN DBs should be listening
+#   Defaults to '0.0.0.0'
+#
+class ovn::northd($dbs_listen_ip = "0.0.0.0") {
     include ::ovn::params
+
+    file { '/etc/sysconfig/ovn-northd':
+      ensure  => file,
+      mode    => '0600',
+      owner   => 'root',
+      group   => 'root',
+      content => "NORTHD_OPTS=--db-nb-addr=${dbs_listen_ip} --db-sb-addr=${dbs_listen_ip}",
+      before  => Service['northd']
+    }
 
     service { 'northd':
         ensure    => true,
